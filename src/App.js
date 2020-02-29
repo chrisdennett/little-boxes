8eced99341167d9b8198c1f7f1c2b2673ad722d7
chrisdennett <chrisdennett@gmail.com>
2020-02-29 11:05:37 +0000

    Move menu to the bottom on mobile

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import TopBar from "./top-bar/TopBar";
 import Display from "./display/Display";
 import Controls from "./controls/Controls";
 import { getAppData } from "./appData";
+import useWindowDimensions from "./hooks/usWindowDimensions";
 
 export default function App() {
   const [appData, setAppData] = useState(getAppData());
   const [optionsVisible, setOptionsVisible] = useState(true);
+  const { width } = useWindowDimensions();
+
+  const showMenuOnLeft = width > 700;
 
   return (
     <Space.ViewPort>
+      {/* TOP BAR - uses size info to hide things */}
       <Space.Top size={60}>
         <Space.Info>
           {sizeInfo => (
@@ -27,12 +32,22 @@ export default function App() {
         </Space.Info>
       </Space.Top>
 
+      {/* HOLDER for menu and main panel */}
       <Space.Fill>
-        {optionsVisible && (
+        {/* MENU */}
+        {optionsVisible && showMenuOnLeft && (
           <Space.LeftResizable size={200} scrollable={true}>
-            <Controls onUpdate={setAppData} appData={appData} />
+            <Controls onUpdate={setAppData} appData={appData} wrap={false} />
           </Space.LeftResizable>
         )}
+
+        {optionsVisible && !showMenuOnLeft && (
+          <Space.BottomResizable size={"40%"} scrollable={true}>
+            <Controls onUpdate={setAppData} appData={appData} wrap={true} />
+          </Space.BottomResizable>
+        )}
+
+        {/* MAIN CONTENT */}
         <Space.Fill
           trackSize={true}
           style={{
